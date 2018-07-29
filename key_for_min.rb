7dc79a8d93b442469f06186a2860288cb826c938
Haley Someone <hbarlar1@gmail.com>
2018-07-29 13:23:28 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,9 +8,8 @@ def key_for_min_value(name_hash)
     min_value = name_hash.first[1]
     min_key = name_hash.first[0]
     name_hash.each do |k, v|
-    if v < min_value
-      min_key = k 
-      x = name_hash[x]
+      if v < min_value
+        min_key = k 
     end
   end
 x 
